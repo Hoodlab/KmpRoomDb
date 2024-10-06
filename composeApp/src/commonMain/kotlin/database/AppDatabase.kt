bdf79077ec770cdb960da31055372690e621779a
Andres <android.sjsu@gmail.com>
2024-10-06 10:20:37 -0700

    Fix iOS build.
    The Note iOS app runs on iPhone test device.
    
    - Update Kotlin 2.0.0 -> 2.0.20
    - Update room 2.7.0-alpha04 -> 2.7.0-alpha08
    - Update ksp 2.0.0-1.0.21 -> 2.0.20-1.0.25
    
    Following [Google's Room documentation](https://developer.android.com/kotlin/multiplatform/room#ios)

--- composeApp/src/commonMain/kotlin/database/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/database/AppDatabase.kt
@@ -1,7 +1,9 @@
 package database
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import database.dao.NoteDao
 import database.entity.Note
@@ -9,6 +11,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 
 @Database(entities = [Note::class], version = 1)
+@ConstructedBy(NoteDatabaseConstructor::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 }
@@ -20,4 +23,9 @@ fun getRoomDatabase(
         .setDriver(BundledSQLiteDriver())
         .setQueryCoroutineContext(Dispatchers.IO)
         .build()
+}
+
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object NoteDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
+    override fun initialize(): AppDatabase
 }
